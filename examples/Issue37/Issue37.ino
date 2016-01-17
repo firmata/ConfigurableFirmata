6d8a3b627e21c2fd7c8ba7ddc0bf6922707a3903
Jeff Hoefs <jeff.hoefs@gmail.com>
2016-01-17 11:29:20 -0800

    add free memory logging to issue37 debug sketch

---
+++ examples/Issue37/Issue37.ino
@@ -0,0 +1,365 @@
+/*
+  Firmata is a generic protocol for communicating with microcontrollers
+  from software on a host computer. It is intended to work with
+  any host computer software package.
+
+  To download a host software package, please clink on the following link
+  to open the download page in your default browser.
+
+  https://github.com/firmata/ConfigurableFirmata#firmata-client-libraries
+
+  Copyright (C) 2006-2008 Hans-Christoph Steiner.  All rights reserved.
+  Copyright (C) 2010-2011 Paul Stoffregen.  All rights reserved.
+  Copyright (C) 2009 Shigeru Kobayashi.  All rights reserved.
+  Copyright (C) 2013 Norbert Truchsess. All rights reserved.
+  Copyright (C) 2014 Nicolas Panel. All rights reserved.
+  Copyright (C) 2009-2015 Jeff Hoefs.  All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  See file LICENSE.txt for further informations on licensing terms.
+
+  Last updated by Jeff Hoefs: November 22nd, 2015
+*/
+
+#include <ArduinoUnit.h>
+#include "ConfigurableFirmata.h"
+
+long memCheckCounter = 0;
+char buffer[20];
+
+/*
+ * by default Firmata uses the Serial-port (over USB) of Arduino.
+ * ConfigurableFirmata may also comunicate over ethernet using tcp/ip.
+ * To configure this 'Network Firmata' to use the original WIZ5100-based
+ * ethernet-shield or Arduino Ethernet uncomment the includes of 'SPI.h' and 'Ethernet.h':
+ */
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+/*
+ * To configure 'Network Firmata' to use an ENC28J60 based board include
+ * 'UIPEthernet.h' (no SPI.h required). The UIPEthernet-library can be downloaded
+ * from: https://github.com/ntruchsess/arduino_uip
+ */
+
+//#include <UIPEthernet.h>
+
+/*
+ * To execute Network Firmata on Yun uncomment Bridge.h and YunClient.h.
+ * Do not include Ethernet.h or SPI.h in this case.
+ * On Yun there's no need to configure local_ip and mac in the sketch
+ * as this is configured on the linux-side of Yun.
+ */
+
+//#include <Bridge.h>
+//#include <YunClient.h>
+
+#if defined ethernet_h || defined UIPETHERNET_H || defined _YUN_CLIENT_H_
+/*==============================================================================
+ * Network configuration for Network Firmata
+ *============================================================================*/
+#define NETWORK_FIRMATA
+//replace with ip of server you want to connect to, comment out if using 'remote_host'
+#define remote_ip IPAddress(192, 168, 1, 1)  
+//replace with hostname of server you want to connect to, comment out if using 'remote_ip'
+// #define remote_host "raspi"
+
+//replace with the port that your server is listening on
+#define remote_port 4040
+
+//replace with arduinos ip-address. Comment out if Ethernet-startup should use dhcp. Is ignored on Yun
+#define local_ip IPAddress(192, 168, 1, 2) 
+
+//replace with ethernet shield mac. It's mandatory every device is assigned a unique mac. Is ignored on Yun
+const byte mac[] = {0x99, 0xA2, 0xDA, 0x00, 0x53, 0xE5};
+#endif
+
+// To configure, save this file to your working directory so you can edit it
+// then comment out the include and declaration for any features that you do
+// not need below.
+
+// WARNING: Including all of the following features (especially if also using Ethernet) may exceed
+// the Flash and/or RAM of lower memory boards such as the Arduino Uno or Leonardo.
+
+#include <DigitalInputFirmata.h>
+DigitalInputFirmata digitalInput;
+
+#include <DigitalOutputFirmata.h>
+DigitalOutputFirmata digitalOutput;
+
+#include <AnalogInputFirmata.h>
+AnalogInputFirmata analogInput;
+
+//#include <AnalogOutputFirmata.h>
+//AnalogOutputFirmata analogOutput;
+
+//#include <Servo.h>
+//#include <ServoFirmata.h>
+//ServoFirmata servo;
+// ServoFirmata depends on AnalogOutputFirmata
+#if defined ServoFirmata_h && ! defined AnalogOutputFirmata_h
+#error AnalogOutputFirmata must be included to use ServoFirmata
+#endif
+
+#include <Wire.h>
+#include <I2CFirmata.h>
+I2CFirmata i2c;
+
+//#include <OneWireFirmata.h>
+//OneWireFirmata oneWire;
+
+//#include <StepperFirmata.h>
+//StepperFirmata stepper;
+
+#include <SerialFirmata.h>
+SerialFirmata serial;
+
+#include <FirmataExt.h>
+FirmataExt firmataExt;
+
+//#include <FirmataScheduler.h>
+//FirmataScheduler scheduler;
+
+// To add Encoder support you must first install the FirmataEncoder and Encoder libraries:
+// https://github.com/firmata/FirmataEncoder
+// https://www.pjrc.com/teensy/td_libs_Encoder.html
+// #include <Encoder.h>
+// #include <FirmataEncoder.h>
+// FirmataEncoder encoder;
+
+
+// dependencies. Do not comment out the following lines
+#if defined AnalogOutputFirmata_h || defined ServoFirmata_h
+#include <AnalogWrite.h>
+#endif
+
+#if defined AnalogInputFirmata_h || defined I2CFirmata_h || defined FirmataEncoder_h
+#include <FirmataReporting.h>
+FirmataReporting reporting;
+#endif
+
+// dependencies for Network Firmata. Do not comment out.
+#ifdef NETWORK_FIRMATA
+#if defined remote_ip && defined remote_host
+#error "cannot define both remote_ip and remote_host at the same time!"
+#endif
+#include <EthernetClientStream.h>
+#ifdef _YUN_CLIENT_H_
+YunClient client;
+#else
+EthernetClient client;
+#endif
+#if defined remote_ip && !defined remote_host
+#ifdef local_ip
+EthernetClientStream stream(client, local_ip, remote_ip, NULL, remote_port);
+#else
+EthernetClientStream stream(client, IPAddress(0, 0, 0, 0), remote_ip, NULL, remote_port);
+#endif
+#endif
+#if !defined remote_ip && defined remote_host
+#ifdef local_ip
+EthernetClientStream stream(client, local_ip, IPAddress(0, 0, 0, 0), remote_host, remote_port);
+#else
+EthernetClientStream stream(client, IPAddress(0, 0, 0, 0), IPAddress(0, 0, 0, 0), remote_host, remote_port);
+#endif
+#endif
+#endif
+
+/*==============================================================================
+ * FUNCTIONS
+ *============================================================================*/
+
+void systemResetCallback()
+{
+  // initialize a defalt state
+
+  // pins with analog capability default to analog input
+  // otherwise, pins default to digital output
+  for (byte i = 0; i < TOTAL_PINS; i++) {
+    if (IS_PIN_ANALOG(i)) {
+#ifdef AnalogInputFirmata_h
+      // turns off pullup, configures everything
+      Firmata.setPinMode(i, PIN_MODE_ANALOG);
+#endif
+    } else if (IS_PIN_DIGITAL(i)) {
+#ifdef DigitalOutputFirmata_h
+      // sets the output to 0, configures portConfigInputs
+      Firmata.setPinMode(i, OUTPUT);
+#endif
+    }
+  }
+
+#ifdef FirmataExt_h
+  firmataExt.reset();
+#endif
+}
+
+/*==============================================================================
+ * SETUP()
+ *============================================================================*/
+
+void setup()
+{
+#ifdef NETWORK_FIRMATA
+#ifdef _YUN_CLIENT_H_
+  Bridge.begin();
+#else
+#ifdef local_ip
+  Ethernet.begin((uint8_t *)mac, local_ip); //start ethernet
+#else
+  Ethernet.begin((uint8_t *)mac); //start ethernet using dhcp
+#endif
+#endif
+  delay(1000);
+#endif
+  Firmata.setFirmwareVersion(FIRMWARE_MAJOR_VERSION, FIRMWARE_MINOR_VERSION);
+
+#ifdef FirmataExt_h
+#ifdef DigitalInputFirmata_h
+  firmataExt.addFeature(digitalInput);
+#endif
+#ifdef DigitalOutputFirmata_h
+  firmataExt.addFeature(digitalOutput);
+#endif
+#ifdef AnalogInputFirmata_h
+  firmataExt.addFeature(analogInput);
+#endif
+#ifdef AnalogOutputFirmata_h
+  firmataExt.addFeature(analogOutput);
+#endif
+#ifdef ServoFirmata_h
+  firmataExt.addFeature(servo);
+#endif
+#ifdef I2CFirmata_h
+  firmataExt.addFeature(i2c);
+#endif
+#ifdef OneWireFirmata_h
+  firmataExt.addFeature(oneWire);
+#endif
+#ifdef StepperFirmata_h
+  firmataExt.addFeature(stepper);
+#endif
+#ifdef SerialFirmata_h
+  firmataExt.addFeature(serial);
+#endif
+#ifdef FirmataReporting_h
+  firmataExt.addFeature(reporting);
+#endif
+#ifdef FirmataScheduler_h
+  firmataExt.addFeature(scheduler);
+#endif
+#ifdef FirmataEncoder_h
+  firmataExt.addFeature(encoder);
+#endif
+#endif
+  /* systemResetCallback is declared here (in ConfigurableFirmata.ino) */
+  Firmata.attach(SYSTEM_RESET, systemResetCallback);
+
+  // Network Firmata communicates with Ethernet-shields over SPI. Therefor all
+  // SPI-pins must be set to PIN_MODE_IGNORE. Otherwise Firmata would break SPI-communication.
+  // add Pin 10 and configure pin 53 as output if using a MEGA with Ethernetshield.
+  // No need to ignore pin 10 on MEGA with ENC28J60, as here pin 53 should be connected to SS:
+#ifdef NETWORK_FIRMATA
+  // ignore SPI and pin 4 that is SS for SD-Card on Ethernet-shield
+  for (byte i = 0; i < TOTAL_PINS; i++) {
+    if (IS_PIN_SPI(i)
+        || 4 == i  // SD-Card on Ethernet-shiedl uses pin 4 for SS
+        || 10 == i // Ethernet-shield uses pin 10 for SS
+       ) {
+      Firmata.setPinMode(i, PIN_MODE_IGNORE);
+    }
+  }
+  //  pinMode(PIN_TO_DIGITAL(53), OUTPUT); configure hardware-SS as output on MEGA
+  pinMode(PIN_TO_DIGITAL(4), OUTPUT); // switch off SD-card bypassing Firmata
+  digitalWrite(PIN_TO_DIGITAL(4), HIGH); // SS is active low;
+
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  pinMode(PIN_TO_DIGITAL(53), OUTPUT); // configure hardware SS as output on MEGA
+#endif
+
+  // start up Network Firmata:
+  Firmata.begin(stream);
+#else
+  // start up the default Firmata using Serial interface:
+  Firmata.begin(57600);
+#endif
+  systemResetCallback();  // reset to default config
+}
+
+/*==============================================================================
+ * LOOP()
+ *============================================================================*/
+void loop()
+{
+
+  /* STREAMREAD - processing incoming messagse as soon as possible, while still
+   * checking digital inputs.  */
+  while (Firmata.available()) {
+    Firmata.processInput();
+#ifdef FirmataScheduler_h
+    if (!Firmata.isParsingMessage()) {
+      goto runtasks;
+    }
+  }
+  if (!Firmata.isParsingMessage()) {
+runtasks: scheduler.runTasks();
+#endif
+  }
+
+#if defined NETWORK_FIRMATA && !defined local_ip &&!defined _YUN_CLIENT_H_
+  if (Ethernet.maintain()) {
+    stream.maintain(Ethernet.localIP());
+  } else {
+    return; // don't read or write any data while disconnected
+  }  
+#endif
+
+#ifdef DigitalInputFirmata_h
+  /* DIGITALREAD - as fast as possible, check for changes and output them to the
+   * stream buffer using Firmata.write()  */
+  digitalInput.report();
+#endif
+
+  /* SEND STREAM WRITE BUFFER - TO DO: make sure that the stream buffer doesn't go over
+   * 60 bytes. use a timer to sending an event character every 4 ms to
+   * trigger the buffer to dump. */
+
+#ifdef FirmataReporting_h
+  if (reporting.elapsed()) {
+#ifdef AnalogInputFirmata_h
+    /* ANALOGREAD - do all analogReads() at the configured sampling interval */
+    analogInput.report();
+#endif
+#ifdef I2CFirmata_h
+    // report i2c data for all device with read continuous mode enabled
+    i2c.report();
+#endif
+#ifdef FirmataEncoder_h
+    // report encoders positions if reporting enabled.
+    encoder.report();
+#endif
+
+
+    // send memory reading approximately every 10 minutes
+    // 31579 * 19 (default sampling interval) = 600000 ms
+    if (memCheckCounter++ == 31579) {
+      sprintf(buffer, "free = %u", freeMemory());
+      Firmata.sendString(buffer);
+      memCheckCounter = 0;
+    }
+
+
+  }
+#endif
+#ifdef StepperFirmata_h
+  stepper.update();
+#endif
+#ifdef SerialFirmata_h
+  serial.update();
+#endif
+}
