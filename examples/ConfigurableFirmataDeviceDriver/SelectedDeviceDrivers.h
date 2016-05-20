8b4e215afcdf2f9c883102b89fe195b8223c7139
Doug Johnson <finson@whidbey.com>
2016-05-19 20:35:26 -0700

    Change the example set of device drivers.

--- examples/ConfigurableFirmataDeviceDriver/SelectedDeviceDrivers.h
+++ examples/ConfigurableFirmataDeviceDriver/SelectedDeviceDrivers.h
@@ -2,19 +2,21 @@
 
 // Device Drivers
 
-//#include <DDHello/DDHello.h>
-#include <DDMCP9808/DDMCP9808.h>
 #include <DDMeta/DDMeta.h>
-//#include <DDSensor/DDSensor.h>
+#include <DDHello/DDHello.h>
+//#include <DDMCP9808/DDMCP9808.h>
 //#include <DDServo/DDServo.h>
+
+//#include <DDSensor/DDSensor.h>
 //#include <DDStepper/DDStepper.h>
 
 DeviceDriver *selectedDevices[] = {
-//  new DDHello("Hello",1),
-  new DDMCP9808("MCP9808",1,0x18),
   new DDMeta("Meta",1),
-//  new DDSensor("Chan",16),
+  new DDHello("Hello",1),
+//  new DDMCP9808("MCP9808",1,0x18),
 //  new DDServo("Servo",2),
+
+//  new DDSensor("Chan",16),
 //  new DDStepper("Stepper",6),
   0};
 
