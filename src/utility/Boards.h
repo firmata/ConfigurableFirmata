32f76b77b8a2db23931d19adbebe264b2a8dba0e
Patrick Grawehr <pgrawehr@hotmail.com>
2021-07-09 20:29:21 +0200

    Add automatic test for ESP32 build

--- src/utility/Boards.h
+++ src/utility/Boards.h
@@ -697,7 +697,7 @@ writePort(port, value, bitmask):  Write an 8 bit port.
 #define IS_PIN_ANALOG(p)        ((p) == 0 || (p) == 2 || (p) == 4 || ((p) >= 12 && (p) < 16) || ((p >= 25 && (p) < 28) || ((p) >= 32 && (p) < 37) || (p) == 39))
 #define IS_PIN_PWM(p)           (IS_PIN_DIGITAL(p) && digitalPinHasPWM(p))
 #define IS_PIN_SERVO(p)         IS_PIN_DIGITAL(p)
-#define IS_PIN_I2C(p)           (IS_PIN_DIGITAL(p) && (p == 21) || (p == 22) )
+#define IS_PIN_I2C(p)           ((IS_PIN_DIGITAL(p)) && ((p == 21) || (p == 22)))
 #define IS_PIN_SPI(p)           (IS_PIN_DIGITAL(p) && digitalPinHasSPI(p))
 #define IS_PIN_INTERRUPT(p)     (digitalPinToInterrupt(p) > NOT_AN_INTERRUPT)
 #define IS_PIN_SERIAL(p)        (digitalPinHasSerial(p))
