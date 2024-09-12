36c3fb3e41ff0ea17bd2bff92352de75c9ee4273
Patrick Grawehr <pgrawehr@hotmail.com>
2024-09-12 21:32:16 +0200

    Fix build for UnoR4 Wifi/Minima
    Build was broken possibly due to a change in the
    core libraries.

--- src/utility/Boards.h
+++ src/utility/Boards.h
@@ -808,13 +808,8 @@ static inline void attachInterrupt(pin_size_t interruptNumber, voidFuncPtr callb
 #define TOTAL_ANALOG_PINS       8
 #define TOTAL_PINS              22 // 14 digital + 8 analog
 #endif
-// These have conflicting(?) definitions in the core for this CPU
-#undef IS_PIN_PWM
-#undef IS_PIN_ANALOG
 #define VERSION_BLINK_PIN       13
 #define IS_PIN_DIGITAL(p)       ((p) >= 2 && (p) <= 19)
-#define IS_PIN_ANALOG(p)        ((p) >= 14 && (p) < 14 + TOTAL_ANALOG_PINS)
-#define IS_PIN_PWM(p)           digitalPinHasPWM(p)
 #define IS_PIN_SERVO(p)         (IS_PIN_DIGITAL(p) && (p) - 2 < MAX_SERVOS)
 #define IS_PIN_I2C(p)           ((p) == 18 || (p) == 19)
 #define IS_PIN_SPI(p)           ((p) == SS || (p) == MOSI || (p) == MISO || (p) == SCK)
